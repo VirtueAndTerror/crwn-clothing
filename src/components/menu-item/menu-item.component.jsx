98ad0bbd5fb9c815098075052ba4c9874d223cfa
Arnaldo Diaz <arnaldo.e.diaz@gmail.com>
2019-08-29 19:31:45 +0200

    update my menu-items to nav withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+// 'withRouter' is Highter Order Component, that is a function component which accepts another component as a argument and returns a new modified component.
+
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
 //We are destructuring 'title', 'imageUrl' and 'size' form 'props-object'.
-const MenuItem = ({title, imageUrl, size}) => ( 
-  <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => ( 
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+  
     <div className='background-image'style={{
       backgroundImage: `url(${imageUrl})`
     }} />
@@ -15,4 +20,4 @@ const MenuItem = ({title, imageUrl, size}) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
