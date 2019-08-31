c9416467fd38a092813b0e4b2144449f7de42fc7
Arnaldo Diaz <arnaldo.e.diaz@gmail.com>
2019-08-31 16:23:59 +0200

    made the form-input component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import './header.styles.scss';
 
 const Header = () => (
   <div className="header">
-    <Link className="logo-containter" to="/">
+    <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
     <div className="options">
